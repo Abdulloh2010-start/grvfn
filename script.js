9f97e52374c4bc9081669a79156b4afdc653a819
Abdulloh2010-start <abdullohlalala@gmail.com>
2024-12-11 19:47:14 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+var Abdulloh = 5;
+let Islomjon = 'Miye yo'
+const Zarina = 155;
+
+let a = prompt('Ismingizni kiriting');
+let b = prompt('Familiyangizni kiriting');
+let c = prompt('Yoshingizni kiriting');
+let d = prompt('Yoqtirgan faningizni yozing');
+let e = prompt('Yashaydigan tumaningizni kiriting');
+alert('Malumotlaringizni kiritganingiz uchun tashakkur');
+console.log(`Ism:${a}`)
+console.log(`Familiya:${b}`)
+console.log(`Yosh:${c}`)
+console.log(`Yoqtirgan fan:${d}`)
+console.log(`Yashash manzili:${e}`)
